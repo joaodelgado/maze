4f4cd73c185dddfd5ddcc6c57f5e264cee099f3c
João Delgado <joaomtdelgado@gmail.com>
2018-04-08 21:55:11 +0100

    Implement A*

--- src/solver.rs
+++ src/solver.rs
@@ -9,12 +9,13 @@ pub enum SolverType {
     DFS,
     BFS,
     Dijkstra,
+    AStar,
 }
 
 impl SolverType {
     /// A list of possible variants in `&'static str` form
-    pub fn variants() -> [&'static str; 3] {
-        ["dfs", "bfs", "dijkstra"]
+    pub fn variants() -> [&'static str; 4] {
+        ["dfs", "bfs", "dijkstra", "astar"]
     }
 
     pub fn init(&self, maze: &Maze) -> Box<Solver> {
@@ -22,6 +23,7 @@ impl SolverType {
             SolverType::DFS => Box::new(DFS::new(maze)),
             SolverType::BFS => Box::new(BFS::new(maze)),
             SolverType::Dijkstra => Box::new(Dijkstra::new(maze)),
+            SolverType::AStar => Box::new(AStar::new(maze)),
         }
     }
 }
@@ -34,6 +36,7 @@ impl FromStr for SolverType {
             "dfs" => Ok(SolverType::DFS),
             "bfs" => Ok(SolverType::BFS),
             "dijkstra" => Ok(SolverType::Dijkstra),
+            "astar" => Ok(SolverType::AStar),
             _ => Err(Error::UnsupportedSolver(s.to_string())),
         }
     }
@@ -187,7 +190,7 @@ impl Solver for BFS {
 #[derive(Debug, Clone)]
 struct DijkstraNode {
     coord: Coord,
-    dist: u64,
+    dist: u32,
     previous: Option<Box<DijkstraNode>>,
 }
 
@@ -263,6 +266,112 @@ impl Solver for Dijkstra {
             maze.highlight_dark.insert(neighbour);
             match self.queue
                 .binary_search_by_key(&new_neighbour.dist, |n| n.dist)
+            {
+                Ok(pos) | Err(pos) => {
+                    self.queue.insert(pos, new_neighbour);
+                }
+            }
+        }
+
+        if self.queue.is_empty() {
+            return Err(Error::ImpossibleMaze);
+        }
+        self.current = self.queue.remove(0);
+        maze.highlight_dark.remove(&self.current.coord);
+
+        maze.highlight_bright.insert(self.current.coord);
+        self.highlight_path(maze);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone)]
+struct AStarNode {
+    coord: Coord,
+    dist: u32,
+    score: u32,
+    previous: Option<Box<AStarNode>>,
+}
+
+#[derive(Debug)]
+pub struct AStar {
+    current: AStarNode,
+    goal: Coord,
+    queue: Vec<AStarNode>,
+}
+
+impl AStar {
+    fn new(maze: &Maze) -> AStar {
+        AStar {
+            current: AStarNode {
+                coord: maze.start,
+                dist: 0,
+                score: 0,
+                previous: None,
+            },
+            goal: maze.end,
+            queue: vec![],
+        }
+    }
+
+    fn heuristic(&self, coord: &Coord) -> u32 {
+        self.goal.manhattan_dist(coord)
+    }
+
+    fn available_neighbours(&self, maze: &Maze) -> Vec<(Coord, Direction)> {
+        if maze.end == self.current.coord {
+            return Vec::new();
+        }
+
+        maze.connected_neighbours(&self.current.coord)
+            .into_iter()
+            .filter(|(c, _)| !maze.explored.contains(&c))
+            .filter(|(c, _)| {
+                !self.queue
+                    .iter()
+                    .filter(|hc| hc.coord == *c)
+                    .next()
+                    .is_some()
+            })
+            .collect()
+    }
+
+    fn highlight_path_from_cell(hc: &AStarNode, maze: &mut Maze) {
+        maze.highlight_medium.insert(hc.coord);
+        if let Some(ref previous) = hc.previous {
+            AStar::highlight_path_from_cell(&previous, maze);
+        }
+    }
+
+    fn highlight_path(&self, maze: &mut Maze) {
+        maze.highlight_medium.clear();
+        AStar::highlight_path_from_cell(&self.current, maze);
+    }
+}
+
+impl Solver for AStar {
+    fn is_done(&self) -> bool {
+        self.goal == self.current.coord
+    }
+
+    fn tick(&mut self, maze: &mut Maze) -> Result<()> {
+        maze.highlight_bright.clear();
+
+        maze.explored.insert(self.current.coord);
+        for (neighbour, _) in self.available_neighbours(maze).into_iter() {
+            let dist_to_neighbour = self.current.dist + 1;
+
+            let new_neighbour = AStarNode {
+                coord: neighbour,
+                dist: dist_to_neighbour,
+                score: dist_to_neighbour + self.heuristic(&neighbour),
+                previous: Some(Box::new(self.current.clone())),
+            };
+
+            maze.highlight_dark.insert(neighbour);
+            match self.queue
+                .binary_search_by_key(&new_neighbour.score, |n| n.score)
             {
                 Ok(pos) | Err(pos) => {
                     self.queue.insert(pos, new_neighbour);
